e394b9e6aa382fd8621754b399a32a1b258f67da
Miku AuahDark <auahdark687291@gmail.com>
2020-10-10 23:38:44 +0800

    Congratulations, check the commit changes.

--- pwa/sks.js
+++ pwa/sks.js
@@ -1,43 +1,66 @@
 "use strict"
 
-function hitungInsentif(sks)
+function oddEven(N)
+{
+	let ret = []
+
+	for (let i = 1; i <= N; i++)
+		ret[i - 1] = (i + (i % 2 == 0 ? " Genap" : " Ganjil"))
+	
+	return ret
+}
+
+function sumByArray(A)
+{
+	let sum = 0
+
+	for (let i = 0; i < A.length; i++)
+		sum += A[i]
+
+	return sum
+}
+
+function sumByNumber(N)
+{
+	let sum = 0
+
+	for (let i = 1; i <= N; i++)
+		sum += i
+
+	return sum
+}
+
+function powerSumN(x, n)
+{
+	let sum = 0
+
+	for (let i = 0; i <= n; i++)
+		sum += Math.pow(-1, i) * Math.pow(x, i)
+
+	return sum
+}
+
+function multiplicationTable(N)
+{
+	let ret = []
+
+	for (let i = 1; i <= 10; i++)
+		ret[i - 1] = (i + (i < 10 ? " " : "") + " x " + N + " = " + (i * N))
+	
+	return ret
+}
+
+function fibonacci(n)
+{
+	let ret = [0, 1]
+
+	for (let i = 0; i < n; i++)
+		ret[i + 2] = ret[i] + ret[i + 1]
+	
+	return ret
+}
+
+function incentiveCalc(sks)
 {
 	return sks < 8 ? -200 : (1600 + (sks - 8) * 100)
-}
-
-if (typeof window == "undefined")
-{
-	// Node.js
-	const readline = require("readline")
-
-	let r = readline.createInterface({input: process.stdin, output: process.stdout})
-
-	r.question("Masukkan SKS: ", (n) => {
-		let num = Number(n)
-
-		if (num == num) // NaN check
-		{
-			let insentif = hitungInsentif(num)
-		
-			console.log((insentif < 0 ? "Bayar denda: $" : "Insentif: $") + insentif)
-			r.close()
-		}
-	})
-}
-else
-{
-	// Browser
-	while (true)
-	{
-		let n = prompt("Masukkan SKS")
-		let num = Number(n)
-
-		if (num == num) // NaN check
-		{
-			let insentif = hitungInsentif(num)
-		
-			alert((insentif < 0 ? "Bayar denda: $" : "Insentif: $") + insentif)
-			break
-		}
-	}
 }
